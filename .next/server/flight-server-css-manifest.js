e773458295c114f0e5beb751006c27ba67e0f4fe
Clizio Guedes <clizioguedes@ioasys.com.br>
2023-04-16 11:11:39 -0300

    feature: first commit with nextjs

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/clizioguedes/Dev/football-results-nextjs/src/app/page.tsx": [
+      "/Users/clizioguedes/Dev/football-results-nextjs/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/clizioguedes/Dev/football-results-nextjs/src/app/page": []
+  }
+}
